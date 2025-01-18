db540cd1fd3f2d5bea462e596188648c7ab35a31
Bryson Fossati <brysonfossati@gmail.com>
2025-01-17 19:33:19 -0700

    first commit

---
+++ guesser.py
@@ -0,0 +1,18 @@
+import random
+print("age guesser!")
+print("answer with yes or no")
+
+prev_guessses = []
+guessed = False
+while(guessed == False):
+    guess = random.randint(15,30)
+    if guess in prev_guessses:
+        continue
+
+    user_res = input(f'are you {guess} years old? ')
+    if user_res == 'yes':
+        guessed = True
+        print("im built different and im right wooo")
+    else:
+        print('mannnnn')
+        prev_guessses.append(guess)
